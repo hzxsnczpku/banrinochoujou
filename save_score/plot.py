d08bd88cdcb9d6697c5ff24cf6a31b57e85f640e
hzxsncz <hzxsncz@pku.edu.cn>
2017-11-02 15:30:50 +0800

    pendulum

--- save_score/plot.py
+++ save_score/plot.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pylab
 
-length = 50000
-MEAN_LENGTH = length//100
+length = 10000
+MEAN_LENGTH = length//25
 
 NAME = ["InvertedDoublePendulum-v1", "HalfCheetah-v1", "InvertedPendulum-v1", "Swimmer-v1", "Reacher-v1", "Walker2d-v1",
         "Humanoid-v1", "HumanoidStandup-v1", "Ant-v1", "Hopper-v1", "BipedalWalker-v2"]
 
-index = 0
+index = 8
 
 distri = ["PPO_adapted", "PPO_clip", "TRPO", "A2C"]
 pylab.title(NAME[index])
@@ -22,12 +22,18 @@ for dis in distri:
     lower = []
 
     for i in range(min(a.shape[0], length)):
-        if i < MEAN_LENGTH:
-            mean = np.mean(a[0:i + 1])
-            std = np.std(a[0:i + 1])
-        else:
-            mean = np.mean(a[i - MEAN_LENGTH: i])
-            std = np.std(a[i - MEAN_LENGTH: i])
+        fin = False
+        while not fin:
+            if i < MEAN_LENGTH:
+                mean = np.mean(a[0:i + 1])
+                std = np.std(a[0:i + 1])
+            else:
+                mean = np.mean(a[i - MEAN_LENGTH: i])
+                std = np.std(a[i - MEAN_LENGTH: i])
+            if np.abs(a[i]-mean) > 100 * std:
+                a[i] = mean
+            else:
+                fin = True
         b.append(mean)
         upper.append(mean + std)
         lower.append(mean - std)
